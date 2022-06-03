3be719ff805624e822968d7962a9b8d67b697108
mddbsd <106027231+mddbsd@users.noreply.github.com>
2022-06-02 23:04:22 -0300

    Delete Programacion Java Clase 1 - Variables directory

--- Programacion Java Clase 1 - Variables/04-StingsYChar/UsoDeStrings.java
+++
@@ -1,8 +0,0 @@
-public class UsoDeStrings {
-    public static void main(String[] args){
-        String miPalabra="cerveza";
-        String miFrase="Donde esta mi cerveza?";
-        System.out.println("Una palabra que uso con frecuencia: "+miPalabra);
-        System.out.println("Una frace que uso a veces: "+miFrase);
-    }
-}
