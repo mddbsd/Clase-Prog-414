329191d9e052ee141abe19ecd062707795e4856a
mddbsd <106027231+mddbsd@users.noreply.github.com>
2022-06-02 23:04:33 -0300

    Delete Programacion Java Clase 2 - Entrada por teclado directory

--- Programacion Java Clase 2 - Entrada por teclado/01 Ingreso de datos String/TuNombre.java
+++
@@ -1,8 +0,0 @@
-public class TuNombre {
-    public static void main(String[] args){
-        String nombre;
-        System.out.print("Ingrese su nombre: ");
-        nombre = System.console().readLine();
-        System.out.println("Hola " + nombre);
-    }
-}
